015666c739bbb07271d958507c985c905321e661
Ahmet Bircan Kaya <bbircankaya@gmail.com>
2020-09-26 17:17:10 +0300

    proje yükleniyor

---
+++ app/src/main/java/com/bibu/twitcheryayini/Details.java
@@ -0,0 +1,97 @@
+package com.bibu.twitcheryayini;
+
+
+import com.bibu.twitcheryayini.module.ourWebView;
+import com.bibu.twitcheryayini.R;
+import com.bibu.twitcheryayini.config.admob;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.Menu;
+
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class Details extends AppCompatActivity {
+
+	WebView browser;
+	ActionBar actionBar;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_details);
+		
+		LinearLayout linearlayout = (LinearLayout)findViewById(R.id.Unit_Ads2);
+        admob.admobBannerCall(this, linearlayout);
+        
+		//this.setTitleColor(getResources().getColor(R.color.menuTextcolor));
+		actionBar= this.getActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
+        upArrow.setColorFilter(getResources().getColor(R.color.menuTextcolor), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        this.setTitle(getIntent().getStringExtra("title"));
+        
+		browser = (WebView) findViewById(R.id.webView1);
+		
+		//Toast.makeText(Details.this, "id:"+getIntent().getStringExtra("id"), Toast.LENGTH_SHORT).show();
+		
+		browser.getSettings().setJavaScriptEnabled(true);
+		browser.getSettings().setLoadWithOverviewMode(true);
+		browser.getSettings().setUseWideViewPort(true);
+		browser.setWebViewClient(new ourWebView());
+		
+		final StringBuilder s = new StringBuilder();    
+
+		s.append("<html>");  
+		s.append("<head><meta name='viewport' content='width=device-width, user-scalable=no' >" +
+				"<style>body {line-height: 170%;}</style>" +
+				"</head>");
+		s.append("<body style='padding-bottom:60px'>");
+		s.append(getIntent().getStringExtra("detail"));
+		s.append("</body>");                            
+		s.append("</html>");
+		browser.loadDataWithBaseURL(null, s.toString(), "text/html", "UTF-8", null);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.details, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		/*int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);*/
+		switch (item.getItemId()) {
+        case android.R.id.home:
+            //Toast.makeText(this, "This is my Toast message!",
+                    //Toast.LENGTH_LONG).show();
+            /*Intent intent = new Intent(Details.this, ListViews.class);
+            startActivity(intent);*/
+            this.finish();
+        	//finishActivity(0);
+            return true;
+        default:
+            Toast.makeText(this, "Ayarlar geliştirme aşamasında...",
+                    Toast.LENGTH_LONG).show();
+            return super.onOptionsItemSelected(item);
+    }
+	}
+}
---
+++ app/src/main/java/com/bibu/twitcheryayini/ListViewsItems.java
@@ -0,0 +1,180 @@
+package com.bibu.twitcheryayini;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+
+import com.bibu.twitcheryayini.adapter.ListItemAdapter;
+import com.bibu.twitcheryayini.database.DataBaseHelper;
+import com.bibu.twitcheryayini.module.Item;
+import com.bibu.twitcheryayini.R;
+import com.bibu.twitcheryayini.config.admob;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class ListViewsItems extends Activity {
+    
+	InterstitialAd  mInterstitialAd;
+	AdRequest adRequest;
+    private ListView lvItem;
+    private ListItemAdapter adapter;
+    private List<Item> mItemList;
+    private DataBaseHelper mDBHelper;
+	
+	Button rateus, shareapp;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_view);
+		//View adContainer = findViewById(R.id.unitads);
+		final String packageName = this.getPackageName();
+		
+        LinearLayout linearlayout = (LinearLayout)findViewById(R.id.unitads);
+        admob.admobBannerCall(this, linearlayout);
+        
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(SettingsApp.Interstitial);
+        mInterstitialAd.setAdListener(new AdListener() {
+        	@Override
+        	public void onAdClosed() {
+        		requestNewInterstitial();
+        	}
+		});
+
+        requestNewInterstitial();
+        
+        rateus = (Button)findViewById(R.id.rateus2);
+        shareapp= (Button) findViewById(R.id.play2);
+        
+        rateus.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				AppRater.rateLink(ListViewsItems.this);
+			     
+			}
+		});
+        
+        shareapp.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				ShareApp();
+			}
+		});
+        
+        mDBHelper = new DataBaseHelper(this);
+        lvItem = (ListView)findViewById(R.id.listViewtest);
+        //Check exists database
+        File database = getApplicationContext().getDatabasePath(DataBaseHelper.DBNAME);
+        if(false == database.exists()) {
+            mDBHelper.getReadableDatabase();
+            //Copy db
+            if(copyDatabase(this)) {
+                //Toast.makeText(this, "Copy database succes", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Copy data error"+DataBaseHelper.DBLOCATION, Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+        //Get product list in db when db exists
+        mItemList = mDBHelper.getListItem();
+        //Init adapter
+        adapter = new ListItemAdapter(this, mItemList);
+        //Set adapter for listview
+        lvItem.setAdapter(adapter);
+        itemSelected();
+	}
+	
+	private void ShareApp(){
+		Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+		sharingIntent.setType("text/plain");
+		String shareBody = "Hey bu oyunu denemeye ne dersin :)\n https://play.google.com/store/apps/details?id="+ getPackageName() +" \n";
+		sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject Here");
+		sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+		startActivity(Intent.createChooser(sharingIntent, "Şununla Paylaş"));
+	}
+	
+	private boolean copyDatabase(Context context) {
+        try {
+
+            InputStream inputStream = context.getAssets().open(DataBaseHelper.DBNAME);
+            String outFileName = DataBaseHelper.DBLOCATION + DataBaseHelper.DBNAME;
+            OutputStream outputStream = new FileOutputStream(outFileName);
+            byte[]buff = new byte[1024];
+            int length = 0;
+            while ((length = inputStream.read(buff)) > 0) {
+                outputStream.write(buff, 0, length);
+            }
+            outputStream.flush();
+            outputStream.close();
+            Log.w("MainActivity","DB copied");
+            return true;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+	
+	
+	@Override
+    public void onBackPressed() {
+    	AppRater.app_launched(this);
+    }
+   
+    private void requestNewInterstitial() {
+		 AdRequest adRequest = new AdRequest.Builder().build();
+		 mInterstitialAd.loadAd(adRequest);
+	} 
+  
+    private void itemSelected() {
+    	
+    	lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    		 Item itemClicked = mItemList.get(position)	;
+    		    
+    			try {
+    				//Class classe= Class.forName(getPackageName()+".Details");
+    				Intent intent=new Intent(ListViewsItems.this, Details.class);
+    				intent.putExtra("detail",""+itemClicked.getText());
+    				intent.putExtra("title",""+itemClicked.getTitle());
+    				startActivityForResult(intent, 1);
+    				//startActivity(intent);
+    				//finish();
+    				if(admob.mCount == admob.nbShowInterstitial) {
+    					if (mInterstitialAd.isLoaded()) 
+    						mInterstitialAd.show();
+    						admob.mCount=0;
+    				}
+    				++admob.mCount;
+    			} catch (Exception e) {
+    				// TODO Auto-generated catch block
+    				e.printStackTrace();
+    			}
+    		 
+    		};
+    	});
+    }
+    
+}
